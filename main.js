056652afabd258cb147bca9ff79a330a3aeb13bd
Ivan Okello <ivandamieno99@gmail.com>
2022-10-20 16:51:56 +0300

    Finally completed the design, it wasn't easy

--- main.js
+++ main.js
@@ -27,3 +27,24 @@ new Chart(chart, {
         responsive: true
     }
 })
+
+
+//Show or hide sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+})
+
+//Change theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    themeBtn.querySelector('span:first-child').classList.toggle('active')
+    themeBtn.querySelector('span:last-child').classList.toggle('active')
+})
